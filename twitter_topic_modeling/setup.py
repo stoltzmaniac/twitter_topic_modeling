87919ce2ddbaeb12777689f62f314a6f95757e88
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-08-17 15:01:31 -0600

    Add all cookiecutter

---
+++ twitter_topic_modeling/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Looking at topics being discussed on Twitter',
+    author='Scott Stoltzman',
+    license='MIT',
+)
