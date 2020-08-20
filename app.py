38d2903f806381586ab1b59606bdd9d6d30b3875
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-08-20 17:08:40 -0600

    Utilize power of spacy, need to fix topics

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 import plotly.express as px
 import pandas as pd
 import dotenv
-from twitter_topic_modeling.models.model_topic import get_raw_data, clean_tweet
+from twitter_topic_modeling.models.model_topic import get_raw_data, return_data_type, model_data, display_topics
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
@@ -26,17 +26,17 @@ def home():
 @app.route('/<username>/<n_tweets>/<return_type>')
 def user_tweets(username, n_tweets, return_type):
     raw_data = get_raw_data(username, int(n_tweets))
+    return return_data_type(raw_data, return_type)
 
-    if return_type == 'json':
-        return {i._json['id_str']: i._json for i in raw_data}
 
-    tweet_text_list = [clean_tweet(i._json['text']) for i in raw_data if not i.retweeted]
+@app.route('/topics/<username>')
+def topics(username):
+    raw_data = get_raw_data(username, 20)
+    clean_tweet_list = return_data_type(raw_data, 'list')
+    logger.warning(clean_tweet_list['data'])
+    model = model_data(clean_tweet_list['data'])
+    return display_topics(model)
 
-    if return_type == 'list':
-        return {'data': tweet_text_list}
-
-    if return_type == 'text':
-        return {'data': " ".join(tweet_text_list)}
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
--- twitter_topic_modeling/models/model_topic.py
+++ twitter_topic_modeling/models/model_topic.py
@@ -2,6 +2,8 @@ import dotenv
 import os
 import tweepy
 import spacy
+from sklearn.decomposition import LatentDirichletAllocation, NMF
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from spacymoji import Emoji
 
 
@@ -11,7 +13,7 @@ nlp = spacy.load("en_core_web_sm")
 emoji = Emoji(nlp)
 nlp.add_pipe(emoji)
 
-def get_raw_data(username, n_tweets=20):
+def get_raw_data(username, n_tweets):
     auth = tweepy.OAuthHandler(os.getenv('TWITTER_API_KEY'), os.getenv('TWITTER_API_SECRET_KEY'))
     auth.set_access_token(os.getenv('TWITTER_ACCESS_TOKEN'), os.getenv('TWITTER_ACCESS_SECRET'))
     api = tweepy.API(auth)
@@ -24,51 +26,54 @@ def clean_tweet(text):
     filtered_sentence = [token.lemma_.replace('\n','').replace('RT','').replace('@','').replace(u"\u2018", "").replace(u"\u2019", "") for token in token_list]
     return(" ".join(filtered_sentence))
 
-# def gather_and_model_data(username='realDonaldTrump', n_count=20, no_topics=3, no_features=100):
-#     auth = tweepy.OAuthHandler(os.getenv('TWITTER_API_KEY'), os.getenv('TWITTER_API_SECRET_KEY'))
-#     auth.set_access_token(os.getenv('TWITTER_ACCESS_TOKEN'), os.getenv('TWITTER_ACCESS_SECRET'))
-#     api = tweepy.API(auth)
-#     tweets = api.user_timeline(username, count=n_count)
-#     tweet_text_list = [i._json['text'] for i in tweets]
-#     tweet_text = " ".join(tweet_text_list)
-#
-#     # NMF is able to use tf-idf
-#     tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
-#     tfidf = tfidf_vectorizer.fit_transform(tweet_text_list)
-#     tfidf_feature_names = tfidf_vectorizer.get_feature_names()
-#
-#     # LDA can only use raw term counts for LDA because it is a probabilistic graphical model
-#     tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
-#     tf = tf_vectorizer.fit_transform(tweet_text_list)
-#     tf_feature_names = tf_vectorizer.get_feature_names()
-#
-#     # Run NMF
-#     nmf = NMF(n_components=no_topics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf)
-#
-#     # Run LDA
-#     lda = LatentDirichletAllocation(n_components=no_topics, max_iter=5, learning_method='online', learning_offset=50.,
-#                                     random_state=0).fit(tf)
-#
-#     return {
-#         'nmf': nmf,
-#         'nmf_feature_names': tfidf_feature_names,
-#         'lda': lda,
-#         'lda_feature_names': tf_feature_names
-#     }
-#
-# def display_topics(model, feature_names, no_top_words):
-#     return {
-#         f"{topic_idx}": " ".join(
-#             [
-#                 feature_names[i]
-#                 for i in topic.argsort()[: -no_top_words - 1 : -1]
-#             ]
-#         )
-#         for topic_idx, topic in enumerate(model.components_)
-#     }
-#
-#
-#
-#
+
+def return_data_type(raw_data, return_type):
+    if return_type == 'raw':
+        return {i._json['id_str']: i._json for i in raw_data}
+    tweet_text_list = [clean_tweet(i._json['text']) for i in raw_data if not i.retweeted]
+    if return_type == 'clean_list':
+        return {'data': tweet_text_list}
+    if return_type == 'clean_text':
+        return {'data': " ".join(tweet_text_list)}
+
+
+def model_data(tweet_text_list, no_topics=5, no_features=100, no_top_words=10):
+
+    # NMF is able to use tf-idf
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
+    tfidf = tfidf_vectorizer.fit_transform(tweet_text_list)
+    nmf = NMF(n_components=no_topics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf)
+    tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+
+    # # LDA can only use raw term counts for LDA because it is a probabilistic graphical model
+    # tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
+    # tf = tf_vectorizer.fit_transform(tweet_text_list)
+    # tf_feature_names = tf_vectorizer.get_feature_names()
+    # lda = LatentDirichletAllocation(n_components=no_topics, max_iter=5, learning_method='online', learning_offset=50.,
+    #                                 random_state=0).fit(tf)
+
+    return {'model': nmf, 'feature_names': tfidf, 'no_top_words': no_top_words}
+
+    # return {
+    #     'nmf': nmf,
+    #     'nmf_feature_names': tfidf_feature_names,
+    #     'lda': lda,
+    #     'lda_feature_names': tf_feature_names
+    # }
+
+def display_topics(model):
+    return {
+        f"{topic_idx}": " ".join(
+            [
+                model['feature_names'][i]
+                for i in topic.argsort()[: -model['no_top_words'] - 1 : -1]
+            ]
+        )
+        for topic_idx, topic in enumerate(model['model'].components_)
+    }
+
+
+
+
 
 
